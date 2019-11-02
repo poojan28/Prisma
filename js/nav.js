5c9b10313efdfa64b5007279bd96ce8509c134bd
Poojan-98 <chapagain.pujan@gmail.com>
2019-11-02 19:31:41 +0545

    moving file from gitlab to github

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+$('#toggle').click(function() {
+  $(this).toggleClass('active');
+  $('#overlay').toggleClass('open');
+});
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
